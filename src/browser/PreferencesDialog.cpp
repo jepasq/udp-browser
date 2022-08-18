f6335280ba49127dc66d61d061ff2d1dba16c3c6
Jepasq <jepasq111@gmail.com>
2022-08-18 12:18:53 +0200

    Save both quota values in Preferences

--- src/browser/PreferencesDialog.cpp
+++ src/browser/PreferencesDialog.cpp
@@ -2,6 +2,8 @@
 
 #include "Preferences.hpp"
 
+#include <iostream>
+
 PreferencesDialog::PreferencesDialog()
 {
   ui.setupUi(this);
@@ -13,11 +15,42 @@ PreferencesDialog::load(Preferences* p)
 {
   ui.leUsername->setText(p->getUsername());
   ui.sbQuota->setValue(p->getQuotaNum());
-  // ui.cbQuota
+  auto q = p->getQuotaUnit();
+  QString qus;
+  switch (q) {
+  case QU_KB:
+    qus = "KBytes";
+    break;
+  case QU_MB:
+    qus = "MB";
+    break;
+  case QU_GB:
+    qus = "GB";
+    break;
+  default:
+    std::cout << "Quota unit not valid " << q << std::endl;
+  };
+  
+  ui.cbQuota->setCurrentText(qus);
 }
 
 void
 PreferencesDialog::save(Preferences* p)
 {
   p->setUsername(ui.leUsername->text());
+  auto unit = ui.cbQuota->currentText().toStdString();
+  QuotaUnit_t unn;
+  if (unit == "KBytes")
+    unn = QU_KB;
+  else if (unit == "MB")
+    unn = QU_MB;
+  else if (unit == "GB")
+    unn = QU_GB;
+  else
+    std::cout << "Quota unit not valid " << unit << std::endl;
+    
+    
+  std::cout << "Current quota unit is " << unit
+	    << std::endl;
+  p->setQuota(ui.sbQuota->value(), unn);
 }
