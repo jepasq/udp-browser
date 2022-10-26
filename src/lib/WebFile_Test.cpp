1a7e8fdab971a4b94032e8c0b34328a76e5a13df
Jepasq <jepasq111@gmail.com>
2022-10-26 03:28:19 +0200

    Add failing WebFile::setContent unit test

--- src/lib/WebFile_Test.cpp
+++ src/lib/WebFile_Test.cpp
@@ -14,3 +14,14 @@ BOOST_AUTO_TEST_CASE( WebFile_get_content )
   BOOST_CHECK( wf.getContent().isEmpty());
 }
 
+BOOST_AUTO_TEST_CASE( WebFile_set_content )
+{
+  WebFile wf;
+  auto ct = wf.getContent();
+  QString new_content = "AZEazeazeAZE";
+
+  wf.setContent(new_content);
+  
+  BOOST_CHECK( wf.getContent() != ct);
+}
+
