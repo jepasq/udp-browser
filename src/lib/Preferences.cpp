bc6272eb3e6b20c10a99b427594d7e767b54d5b5
Jepasq <jepasq111@gmail.com>
2022-11-10 05:13:24 +0100

    Improve Preferences class documentation

--- src/lib/Preferences.cpp
+++ src/lib/Preferences.cpp
@@ -15,6 +15,9 @@ Preferences::Preferences(const QString& filen):
 
 }
 
+/** Save the current preferences fields to a new data stream
+  *
+  */
 void
 Preferences::save(void)
 {
@@ -26,6 +29,9 @@ Preferences::save(void)
   Serializer::close();
 }
 
+/** Save the current preferences fields from a newly created data stream
+  *
+  */
 void
 Preferences::load(void)
 {
@@ -36,18 +42,34 @@ Preferences::load(void)
   Serializer::close();
 }
 
+/** Change the current username
+  *
+  * \param u the new name as a QString.
+  *
+  */
 void
 Preferences::setUsername(const QString& u)
 {
   username= u;
 }
 
+/** Get the current username
+  *
+  * \return the current name as a QString. May be an empty string.
+  *
+  */
 const QString&
 Preferences::getUsername(void) const
 {
   return username;
 }
 
+/** Set the current quota-related members
+  *
+  * \param num  the numeric part of the quota.
+  * \param unit the unit part of the quota.
+  *
+  */
 void
 Preferences::setQuota(unsigned int num, QuotaUnit_t unit)
 {
@@ -55,12 +77,22 @@ Preferences::setQuota(unsigned int num, QuotaUnit_t unit)
   quotaUnit = unit;
 }
 
+/** Get the numeric part of the quota
+  *
+  * \return An unsigned int.
+  *
+  */
 unsigned int
 Preferences::getQuotaNum(void) const
 {
   return quotaNum;
 }
 
+/** Get the unit part of the quota
+  *
+  * \return The quota unit.
+  *
+  */
 QuotaUnit_t
 Preferences::getQuotaUnit(void) const
 {
