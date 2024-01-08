a770124205e20a547e039ad3f5c4522bc8891df0
Jepasq <jepasq111@gmail.com>
2024-01-08 13:38:13 +0100

    Add NetworkNode::download function and fix compilation issue

--- src/lib/NetworkNode.cpp
+++ src/lib/NetworkNode.cpp
@@ -4,33 +4,46 @@
 
 #include <boost/array.hpp>
 
+/** The NetworkNode constructor
+  *
+  */
 NetworkNode::NetworkNode()
 {
-  try
-    {
-      // From https://www.developpez.net/forums/d722277/c-cpp/cpp/bibliotheques/boost/boost-asio-ecouter-port-udp/#post4192089
-      boost::asio::io_service io_service;
-      /*      udp::endpoint endpoint (boost::asio::ip::address::from_string("134.246.151.185"), 10001);
-      udp::endpoint sender;
-      
-      udp::socket socket(io_service);
-      socket.open(udp::v4());
-      socket.bind(endpoint); // <------
-      
-      boost::array<char, 128> recv_buf;
-      size_t len = socket.receive_from(boost::asio::buffer(recv_buf), sender);
-      
-      std::cout.write(recv_buf.data(), len);
-      */
-      
-    }
-  catch (std::exception& e)
-    {
-      std::cerr << e.what() << std::endl;
-    }
 }
 
 NetworkNode::~NetworkNode()
 {
 
 }
+
+void
+NetworkNode::download(std::string ip = "134.246.151.185", int port = 10001)
+{
+  using boost::asio::ip::udp;
+  
+  udp::endpoint sender;
+  boost::array<char, 128> recv_buf;
+  size_t len;
+  
+  try
+    {
+      // From https://www.developpez.net/forums/d722277/c-cpp/cpp/bibliotheques/boost/boost-asio-ecouter-port-udp/#post4192089
+      udp::endpoint endpoint (boost::asio::ip::address::from_string(ip), port);
+      
+      udp::socket socket(io_service);
+      socket.open(udp::v4());
+      socket.bind(endpoint); // <------
+      
+      len = socket.receive_from(boost::asio::buffer(recv_buf), sender);
+      
+      std::cout.write(recv_buf.data(), len);
+      
+      
+    }
+  catch (std::exception& e)
+    {
+      std::cerr << e.what() << std::endl;
+    }
+
+}
+
--- src/lib/NetworkNode.hpp
+++ src/lib/NetworkNode.hpp
@@ -11,7 +11,12 @@ class NetworkNode
 public:
   NetworkNode();
   ~NetworkNode();
-  
+
+  // Not sure for the return type yet
+  void download(std::string, int);
+
+private:
+  boost::asio::io_service io_service;
   
   // A download URL function 
 };
