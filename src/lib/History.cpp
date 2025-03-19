d053ceca1a5888b0a7693841c23f3add0100e112
Jepasq <jepasq111@gmail.com>
2025-03-19 10:46:23 +0100

    Improve History documentation ans set JAVADOC_AUTOBRIEF to YES

--- src/lib/History.cpp
+++ src/lib/History.cpp
@@ -1,10 +1,21 @@
 #include "History.hpp"
 
+/** Add a new shared HistoryItem based on given url and title
+ *
+ * @param url   The page URL.
+ * @param title The associated title.
+ *
+ */
 void History::add(const std::string& url, const std::string& title)
 {
   items.push_front(std::make_shared<HistoryItem>(url, title));
 }
 
+/** Get current list of items
+ *
+ *  @return A std::list of history items.
+ *
+ */
 std::list<std::shared_ptr<HistoryItem>>
 History::getItems(void) const
 {
